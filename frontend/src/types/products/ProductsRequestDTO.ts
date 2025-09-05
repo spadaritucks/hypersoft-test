6cb3108e6109175e8a45b399e69f1a677d9ce1f5
spadaritucks <thiago.spadari02@gmail.com>
2025-09-05 01:12:16 -0300

    feat(products): add ProductsRequestDTO and ProductsResponseDTO types for product management

---
+++ frontend/src/types/products/ProductsRequestDTO.ts
@@ -0,0 +1,8 @@
+export type ProductsRequestDTO = {
+    name : string,
+    description : string
+    price : number,
+    categoryId : string,
+    stockQuantity : number
+}
+
---
+++ frontend/src/types/products/ProductsResponseDTO.ts
@@ -0,0 +1,10 @@
+export type ProductsResponseDTO = {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    categoryId: string;
+    stockQuantity: number;
+    createdAt: string;
+    updatedAt: string;
+}
