78f4acd260171f69810eccc67d43c3989d291b62
spadaritucks <thiago.spadari02@gmail.com>
2025-09-05 01:16:39 -0300

    feat(login): implement LoginForm component with validation and submission handling

---
+++ frontend/src/components/forms/loginForm.tsx
@@ -0,0 +1,61 @@
+"use client"
+import z from "zod";
+import { Button } from "../ui/button";
+import {InputForm} from "./input/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AuthService } from "@/services/AuthService";
+import { useRouter } from "next/navigation";
+
+
+
+const LoginSchema = z.object({
+    username: z.string().min(1, "Username is required"),
+    password: z.string().min(1, "Password is required")
+})
+
+type LoginFormdata = z.infer<typeof LoginSchema>
+
+export default function LoginForm() {
+
+    const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm<LoginFormdata>({
+        resolver: zodResolver(LoginSchema)
+    })
+
+    const router = useRouter();
+
+    async function onSubmit(data: LoginFormdata) {
+
+        try {
+            await AuthService({
+                username: data.username,
+                password: data.password
+            })
+
+            router.push("/dashboard")
+
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+            alert("Erro ao fazer login " + error);
+        }
+
+    }
+
+
+    return (
+        <form className="w-full flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+            <InputForm
+                label="Username"
+                type="text"
+                placeholder="Enter your username"
+                {...register("username")}
+                error={errors.username ? errors.username.message : undefined} />
+            <InputForm
+                label="Password"
+                type="password"
+                {...register("password")}
+                error={errors.password ? errors.password.message : undefined} />
+            <Button variant="default" disabled={isSubmitting}>Login</Button>
+        </form>
+    )
+}
