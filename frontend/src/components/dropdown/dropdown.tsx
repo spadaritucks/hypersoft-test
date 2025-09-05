1173be8f0fa0015373f0f05910ee463a64dc630f
spadaritucks <thiago.spadari02@gmail.com>
2025-09-05 01:18:14 -0300

    feat(dropdown): add HeaderDropdown component for user account options

---
+++ frontend/src/components/dropdown/dropdown.tsx
@@ -0,0 +1,33 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { ChevronDown } from "lucide-react"
+
+
+
+export default function HeaderDropdown() {
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <div className=" flex items-center gap-2 max-w-34 rounded-xl px-4 py-3  border border-gray-300 cursor-pointer hover:opacity-70 transition-all">
+                    <p className="font-semibold">UnitedMen</p>
+                    <ChevronDown/>
+                </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
